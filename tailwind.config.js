edcf5fb59c03c5855ad617f0103e203173188233
klervicn <corbel.klervi@gmail.com>
2023-07-26 23:53:06 +0200

    Add tailwind and css

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./imports/ui/**/*.{js,html}",
+    "./client/*.html",],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
